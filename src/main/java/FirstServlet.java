fcd0bfe8a01d0654dfd742f5e19a25c7e472267f
yanad <yana.danilova1509@gmail.com>
2023-02-14 11:13:13 +0100

    homework

---
+++ src/main/java/FirstServlet.java
@@ -0,0 +1,18 @@
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(name = "first servlet",urlPatterns = "/first_servlet")
+public class FirstServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("text/html;charset=UTF-8");
+        resp.getWriter().print("<html><body>");
+        resp.getWriter().print("Hello Word");
+        resp.getWriter().print("</body></html>");
+    }
+}
